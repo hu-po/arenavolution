a4042a3e3c96e78f6df600951bbd0bac2a922cad
hu.po <hugo.p.cmu@gmail.com>
2024-01-22 12:57:26 -0600

    bs

---
+++ battle.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Player:
+    name: str
+    elo: int
+    wins: int
+    losses: int
+
+# set random seed
+# generate 2k dataset w/ stable diff
+    
+# for num_players
+# load a random player from .npy file in directory
+# train eval player
+
+# remove losing player
+# duplicate winning player with variation (use LLM to generate variation)
+# 
+
